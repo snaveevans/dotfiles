8e51d6843f1c37016717d590bccf280a33404f6d
Tyler <tyler.evans116@gmail.com>
2019-04-28 12:56:17 -0600

    install Xcode command line tools

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Install Xcode command line tools
+xcode-select --install
+
 # Update Homebrew recipes
 brew update
 
