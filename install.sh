3b44440a19851030dea9d6a942f950de786d017a
Tyler <tyler.evans116@gmail.com>
2019-04-28 12:41:38 -0600

    add install and macOS script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir $HOME/Sites
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source macos.sh
