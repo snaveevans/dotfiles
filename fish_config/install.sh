892772cbe95a4fab21e01df1fa9fe7eb2fea4c1a
Tyler Evans <snaveevans@users.noreply.github.com>
2022-07-19 14:20:24 -0600

    more linux work

--- fish_config/install.sh
+++ fish_config/install.sh
@@ -10,6 +10,8 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 # remove existing configuration for fish
 rm $HOME/.config/fish/fish_plugins
 rm $HOME/.config/fish/*.fish
+
 # link dotfile config
-# TODO verify fish config location
-ln -s $HOME/.dotfiles/fish_config/* $HOME/.config/fish
+mkdir -p $HOME/.config/fish
+ln -s  $HOME/.dotfiles/fish_config/*.fish $HOME/.config/fish
+ln -s  $HOME/.dotfiles/fish_config/fish_plugins $HOME/.config/fish
--- linux/install.sh
+++ linux/install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository -y ppa:regolith-linux/release
 
 sudo apt-get update
 
-sudo apt-get install -yqq fish i3-gaps autojump ripgrep fzf neovim
+sudo apt-get install -yqq fish i3-gaps autojump ripgrep fzf neovim kitty
 
 #curl -o $HOME/ https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
 #sudo dpkg -i  $HOME/bat.deb
@@ -15,10 +15,10 @@ rm -rf $HOME/.config/i3
 mkdir -p $HOME/.config/i3
 ln -s $HOME/.dotfiles/linux/i3.config $HOME/.config/i3/config
 
+rm -rf $HOME/.config/i3status
+mkdir -p $HOME/.config/i3status
+ln -s $HOME/.dotfiles/linux/i3status.config $HOME/.config/i3status/config
+
 # configuration for xmodmap
 rm $HOME/.Xmodmap
 ln -s $HOME/.dotfiles/linux/Xmodmap $HOME/.Xmodmap
-
-# configuration for desktop environment
-rm $HOME/.dmrc
-ln -s $HOME/.dotfiles/linux/dmrc $HOME/.dmrc
