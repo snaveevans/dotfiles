fa56ddd69a93f133649a1ab9f5dbfd2cc2e73b84
Tyler Evans <snaveevans@users.noreply.github.com>
2022-07-19 12:42:01 -0600

    good looking linux config

--- linux/install.sh
+++ linux/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
+sudo add-apt-repository -y ppa:regolith-linux/release
+
 sudo apt-get update
 
-sudo apt-get install fish
-sudo apt-get install i3-gaps
-sudo apt-get install autojump
-sudo apt-get install ripgrep
-sudo apt-get install fzf
-sudo apt-get install neovim
-# sudo apt-get install bat
+sudo apt-get install -yqq fish i3-gaps autojump ripgrep fzf neovim
+
+#curl -o $HOME/ https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
+#sudo dpkg -i  $HOME/bat.deb
+#rm $HOME/bat.deb
 
 # configuration for i3
 rm -rf $HOME/.config/i3
@@ -18,3 +18,7 @@ ln -s $HOME/.dotfiles/linux/i3.config $HOME/.config/i3/config
 # configuration for xmodmap
 rm $HOME/.Xmodmap
 ln -s $HOME/.dotfiles/linux/Xmodmap $HOME/.Xmodmap
+
+# configuration for desktop environment
+rm $HOME/.dmrc
+ln -s $HOME/.dotfiles/linux/dmrc $HOME/.dmrc
