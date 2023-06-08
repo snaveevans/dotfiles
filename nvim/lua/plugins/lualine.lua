cb5fcae92bf9682487f17a8f40fb011f4e65709e
Tyler Evans <snaveevans@users.noreply.github.com>
2023-06-07 22:59:22 -0600

    add copilot

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,34 @@
 return {
   "nvim-lualine/lualine.nvim",
+  optional = true,
+  event = "VeryLazy",
+  -- opts = function(_, opts)
+  --   local Util = require("util")
+  --   local colors = {
+  --     [""] = Util.fg("Special"),
+  --     ["Normal"] = Util.fg("Special"),
+  --     ["Warning"] = Util.fg("DiagnosticError"),
+  --     ["InProgress"] = Util.fg("DiagnosticWarn"),
+  --   }
+  --   table.insert(opts.sections.lualine_x, 2, {
+  --     function()
+  --       local icon = "CO: "
+  --       local status = require("copilot.api").status.data
+  --       return icon .. (status.message or "")
+  --     end,
+  --     cond = function()
+  --       local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+  --       return ok and #clients > 0
+  --     end,
+  --     color = function()
+  --       if not package.loaded["copilot"] then
+  --         return
+  --       end
+  --       local status = require("copilot.api").status.data
+  --       return colors[status.status] or colors[""]
+  --     end,
+  --   })
+  -- end,
   config = function()
     local lualine = require("lualine")
 
---
+++ nvim/lua/util.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
+function M.fg(name)
+  ---@type {foreground?:number}?
+  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+  local fg = hl and hl.fg or hl.foreground
+  return fg and { fg = string.format("#%06x", fg) }
+end
+
+return M
