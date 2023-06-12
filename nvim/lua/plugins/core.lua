0998df72727eef52108ee19f890cd7cb3cfcf3de
Tyler Evans <snaveevans@users.noreply.github.com>
2023-06-12 09:13:40 -0600

    try leap.nvim

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -14,21 +14,21 @@ return {
     config = true,
   },
   {
-    "phaazon/hop.nvim",
-    config = function()
-      local hop = require("hop")
-      local directions = require("hop.hint").HintDirection
-
-      hop.setup({ keys = "etovxqpdygfblzhckisuran" })
-
-      vim.keymap.set("n", "<leader><leader>w", function()
-        hop.hint_words({ direction = directions.AFTER_CURSOR })
-      end, {})
-
-      vim.keymap.set("n", "<leader><leader>b", function()
-        hop.hint_words({ direction = directions.BEFORE_CURSOR })
-      end, {})
-    end,
+    "ggandor/leap.nvim",
+    keys = {
+      {
+        "s",
+        function()
+          require("leap").leap({})
+        end,
+      },
+      {
+        "S",
+        function()
+          require("leap").leap({ backward = true })
+        end,
+      },
+    },
   },
   {
     "ibhagwan/fzf-lua",
