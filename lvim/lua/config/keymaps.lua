7143f67f7313cd8ff62f74a5fc0275f08286608e
Tyler Evans <snaveevans@users.noreply.github.com>
2024-09-13 08:21:59 -0600

    feat: try out lazyvim
    There are some issues with this configuration, lualine is not working
    correctly

---
+++ lvim/lua/config/keymaps.lua
@@ -0,0 +1,37 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+
+local cmd_opts = { noremap = true }
+
+local map = vim.keymap.set
+
+local merge = require("util").merge
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+map("n", "<leader><cr>", ":noh<cr>", merge({ desc = "No highlight" }, cmd_opts))
+map("n", "<leader>fn", ':edit <c-r>=expand("%:.:h")<cr>/', merge({ desc = "New file" }, cmd_opts))
+map("n", "<leader>fv", ':vsplit <c-r>=expand("%:.:h")<cr>/', merge({ desc = "New file (split)" }, cmd_opts))
+map("n", "<C-j>", "<C-d>", opts)
+map("n", "<C-k>", "<C-u>", opts)
+
+-- Visual --
+-- Stay in indent mode
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+map("v", "<leader>fr", ":<C-u>call VisualSelection('', '')<CR>:%s/<C-R>=@/<CR>/", opts)
+
+-- Move text up and down
+map("n", "<M-j>", ":m .+1<CR>==", opts)
+map("n", "<M-k>", ":m .-2<CR>==", opts)
+map("v", "<M-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<M-k>", ":m '<-2<CR>gv=gv", opts)
---
+++ lvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "joshdick/onedark.vim",
+    event = "User LazyColorscheme",
+    opts = {
+      style = "night",
+    },
+    config = function() end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
