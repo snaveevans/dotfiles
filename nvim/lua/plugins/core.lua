3cbe48d4af767860f2acaa6cf5ab8e5fe77e099b
Tyler Evans <snaveevans@users.noreply.github.com>
2024-11-13 15:24:38 -0700

    fix copilot, reduce config

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,7 +1,17 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = { ensure_installed = { "prettier" } },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "lua-language-server",
+        "eslint-lsp",
+        "prettier",
+        "vtsls",
+      },
+    },
   },
   {
     "echasnovski/mini.surround",
@@ -32,91 +42,6 @@ return {
         highlight = "gsh", -- Highlight surrounding
         replace = "gsr", -- Replace surrounding
         update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
-  },
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    module = "persistence",
-    keys = {
-      {
-        "<F5>",
-        function()
-          require("persistence").save()
-        end,
-        mode = "n",
-        desc = "Save Session",
-      },
-      {
-        "<F8>",
-        function()
-          require("persistence").load()
-        end,
-        mode = "n",
-        desc = "Load Session",
-      },
-    },
-    config = function()
-      require("persistence").setup()
-    end,
-  },
-  {
-    "ibhagwan/fzf-lua",
-    opts = {
-      fzf_opts = {
-        ["--layout"] = "default",
-      },
-      files = {
-        rg_opts = [[--color=never --files --no-ignore --hidden --follow -g "!.git" -g "!node_modules" -g "!dist"]],
-      },
-    },
-    cmd = { "Rg" },
-    config = function(_, opts)
-      vim.api.nvim_create_user_command("Rg", function(args)
-        local fargs = table.concat(args.fargs, " ")
-        require("fzf-lua").grep_project({ search = fargs, cmd = "rg --hidden" })
-      end, { nargs = "?" })
-
-      require("fzf-lua").setup(opts)
-    end,
-    keys = {
-      {
-        "<leader>p",
-        function()
-          require("fzf-lua").files()
-        end,
-        desc = "FzF Files",
-      },
-      {
-        "<leader>o",
-        function()
-          require("fzf-lua").buffers()
-        end,
-        desc = "FzF Buffers",
-      },
-      {
-        "s",
-        function()
-          require("fzf-lua").grep_visual()
-        end,
-        mode = "v",
-        desc = "FzF Grep Visual",
-      },
-      {
-        "<leader>fe",
-        function()
-          require("fzf-lua").grep_cword()
-        end,
-        mode = "n",
-        desc = "FzF Grep cword",
-      },
-      {
-        "<leader>s",
-        function()
-          require("fzf-lua").live_grep({ cmd = "rg --hidden" })
-        end,
-        desc = "FzF live Grep",
       },
     },
   },
