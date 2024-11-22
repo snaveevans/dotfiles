da6f9b863b00f178afd72c79aa951a0a33961f7e
Tyler Evans <snaveevans@users.noreply.github.com>
2024-11-21 21:08:44 -0700

    nvim cleanup

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -1,35 +1,17 @@
 local M = {}
 
----@param on_attach fun(client, buffer)
-function M.on_attach(on_attach)
-  vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local buffer = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_attach(client, buffer)
-    end,
-  })
-end
-
-function M.fg(name)
-  ---@type {foreground?:number}?
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-  local fg = hl and hl.fg or hl.foreground
-  return fg and { fg = string.format("#%06x", fg) }
-end
-
 function M.merge(t1, t2)
   t1 = t1 or {}
   t2 = t2 or {}
-  
-    for k, v in pairs(t2) do
-        if (type(v) == "table") and (type(t1[k] or false) == "table") then
-            merge(t1[k], t2[k])
-        else
-            t1[k] = v
-        end
+
+  for k, v in pairs(t2) do
+    if (type(v) == "table") and (type(t1[k] or false) == "table") then
+      M.merge(t1[k], t2[k])
+    else
+      t1[k] = v
     end
-    return t1
+  end
+  return t1
 end
 
 return M
