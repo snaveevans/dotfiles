ba199a98074c5b0691b90dbbe31d2d6aac606b38
Tyler Evans <snaveevans@users.noreply.github.com>
2025-07-07 10:59:47 -0600

    fix nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,19 @@
 return {
   {
-    "joshdick/onedark.vim",
-    event = "User LazyColorscheme",
-    opts = {
-      style = "night",
-    },
-    config = function() end,
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
   },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "rebelot/kanagawa.nvim", priority = 1000 },
+  { "folke/tokyonight.nvim", priority = 1000 },
+  { "EdenEast/nightfox.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
