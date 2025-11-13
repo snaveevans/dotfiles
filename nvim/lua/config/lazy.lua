fb491b5b39146272e0612404e4e4de2946e0cf59
Tyler Evans <snaveevans@users.noreply.github.com>
2025-11-13 08:52:47 -0700

    update lazy vim plugin name

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,7 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
