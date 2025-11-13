99249d9d237e400fb2c99573f251e5c5a3af7efb
Tyler Evans <snaveevans@users.noreply.github.com>
2025-11-13 08:52:00 -0700

    nvim in vscode: experimental

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,14 @@
 -- VSCode Neovim integration setup
 if vim.g.vscode then
+    -- Disable Snacks animations (LazyVim compatibility)
+    vim.g.snacks_animate = false
+
     -- Set up basic options for VSCode
     vim.opt.clipboard = "unnamedplus"
     vim.opt.ignorecase = true
     vim.opt.smartcase = true
     vim.opt.hlsearch = true
     vim.opt.incsearch = true
-
-    -- Load vscode module
-    local vscode = require("vscode")
-
-    -- Set VSCode notifications as default
-    vim.notify = vscode.notify
 
     -- Disable some Neovim features that conflict with VSCode
     vim.opt.backup = false
@@ -24,3 +21,12 @@ end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Load vscode module after plugins are loaded (only in VSCode)
+if vim.g.vscode then
+    vim.schedule(function()
+        local vscode = require("vscode")
+        -- Set VSCode notifications as default
+        vim.notify = vscode.notify
+    end)
+end
