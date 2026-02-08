1ba9ab43a15565a712bc543e571c5af3dfb2c4ae
Tyler Evans <snaveevans@users.noreply.github.com>
2026-02-07 23:33:27 -0700

    fix: bw and add synthetic api env

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# exit immediately if password-manager-binary is already in $PATH
-type password-manager-binary >/dev/null 2>&1 && exit
+# exit immediately if bw is already in $PATH
+type bw >/dev/null 2>&1 && exit
 
 case "$(uname -s)" in
 Darwin)
-  # commands to install password-manager-binary on Darwin
+  # commands to install Bitwarden on Darwin
   brew install --cask bitwarden
   brew install bitwarden-cli
-  export BW_SESSION=$(bw unlock --raw)
   ;;
 Linux)
   # commands to install password-manager-binary on Linux
